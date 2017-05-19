e8a688607f1c4c38aab811fb1c334439ba5ab950
Delmer Izaguirre <delmerespinal2@gmail.com>
2017-05-19 12:58:31 -0600

    que tal?

---
+++ src/aleatorio/Aleatorio.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aleatorio;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Owner
+ */
+public class Aleatorio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Random Rnx = new Random();
+        
+        
+        String[] Nombres = { "Rafa","Ileana","Juan","Jefry","Daniel","Ricardo","Delmer","Oswaldo","Wis","Leonardo","Alberto","Miguel","Gabriel","Carlos"};
+        
+        int[] numeros = new int[14];
+        
+        int Rand = Rnx.nextInt(14);
+        
+        
+        int Cont = 14;
+        
+        while( Cont != 0){
+            
+            
+            
+        }
+        
+         
+    }
+        
+        
+    
+}
